f0f4bc9e703fe6d5ed5dd97bde955bbb55d203a7
Vyacheslav Voronenko <git@voronenko.info>
2017-06-18 22:41:03 +0300

    improve(conemu) added conemu settings to profile

--- init.ps1
+++ init.ps1
@@ -14,7 +14,7 @@ $files="aliases components components-nuget components-shell exports functions M
 #
 $components="console git visualstudio"
 $dotfiles="gitconfig gemrc jshintrc jscsrc editorconfig hgrc"
-$roamingfiles="conemu\conemu.xml"
+$roamingfiles="conemu.xml"
 
 New-Item -ItemType Directory -Force -Path $oldfiles
 New-Item -ItemType Directory -Force -Path $oldfiles\components
@@ -23,29 +23,24 @@ New-Item -ItemType Directory -Force -Path $profileDir\components
 New-Item -ItemType Directory -Force -Path $oldfiles\roaming
 New-Item -ItemType Directory -Force -Path $oldfiles\roaming\conemu
 
-$profileExist = False # Test-Path $profile
+$files.Split(" ") | ForEach {
+   if (Test-Path $profileDir\$_.ps1) { Copy-Item $profileDir\$_.ps1 $oldfiles }
+   echo "New-Item -Path $profileDir\$_.ps1 -ItemType SymbolicLink -Value $dir\$_.ps1"
+   New-Item -Path $profileDir\$_.ps1 -ItemType SymbolicLink -Value $dir\$_.ps1 -force
+}
 
-if (!$profileExist) {
+$components.Split(" ") | ForEach {
+   if (Test-Path $profileDir\components\$_.ps1) { Copy-Item $profileDir\components\$_.ps1 $oldfiles\components }
+   New-Item -Path $profileDir\components\$_.ps1 -ItemType SymbolicLink -Value $dir\components\$_.ps1 -force
+}
 
-  $files.Split(" ") | ForEach {
-    if (Test-Path $profileDir\$_.ps1) { Copy-Item $profileDir\$_.ps1 $oldfiles }
-    echo "New-Item -Path $profileDir\$_.ps1 -ItemType SymbolicLink -Value $dir\$_.ps1"
-    New-Item -Path $profileDir\$_.ps1 -ItemType SymbolicLink -Value $dir\$_.ps1
-  }
+$dotfiles.Split(" ") | ForEach {
+   if (Test-Path C:\Users\$username\.$_) { Copy-Item C:\Users\$username\.$_ $oldfiles\home }
+   New-Item -Path C:\Users\$username\.$_ -ItemType SymbolicLink -Value $dir\home\$_ -force
+}
 
-  $components.Split(" ") | ForEach {
-    if (Test-Path $profileDir\components\$_.ps1) { Copy-Item $profileDir\components\$_.ps1 $oldfiles\components }
-    New-Item -Path $profileDir\components\$_.ps1 -ItemType SymbolicLink -Value $dir\components\$_.ps1
-  }
+$roamingfiles.Split(" ") | ForEach {
+  if (Test-Path C:\Users\$username\AppData\Roaming\$_) { Copy-Item C:\Users\$username\AppData\Roaming\$_ $oldfiles\roaming }
+  New-Item -Path C:\Users\$username\AppData\Roaming\$_ -ItemType SymbolicLink -Value $dir\roaming\$_ -force
+}
 
-  $dotfiles.Split(" ") | ForEach {
-    if (Test-Path C:\Users\$username\.$_) { Copy-Item C:\Users\$username\.$_ $oldfiles\home }
-    New-Item -Path C:\Users\$username\.$_ -ItemType SymbolicLink -Value $dir\home\$_
-  }
-
-  $roamingfiles.Split(" ") | ForEach {
-    if (Test-Path C:\Users\$username\AppData\Roaming\$_) { Copy-Item C:\Users\$username\AppData\Roaming\$_ $oldfiles\roaming }
-    New-Item -Path C:\Users\$username\AppData\Roaming\$_ -ItemType SymbolicLink -Value $dir\$_
-  }
-
-} 
